e59d36d88967905bb3025350a5561e46b7823112
ahmed123hds <kodek334@gmail.com>
2025-07-25 09:21:36 +0500

    Temporary changes for now

--- cmake-3.28.3/Utilities/cmexpat/CMakeFiles/cmexpat.dir/lib/xmltok_ns.c.o.d
+++
@@ -1,3 +0,0 @@
-Utilities/cmexpat/CMakeFiles/cmexpat.dir/lib/xmltok_ns.c.o: \
- /workspace/cmake-3.28.3/Utilities/cmexpat/lib/xmltok_ns.c \
- /usr/include/stdc-predef.h
--- cmake-3.28.3/Utilities/cmlibarchive/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspace/cmake-3.28.3/Utilities/cmlibarchive
-# Build directory: /workspace/cmake-3.28.3/Utilities/cmlibarchive
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libarchive")
